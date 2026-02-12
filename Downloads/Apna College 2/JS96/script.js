452bbb619c8bfada67fe62c443d275fefed8787a
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-12 13:15:47 +0530

    Hello.

---
+++ Downloads/Apna College 2/JS96/script.js
@@ -0,0 +1,15 @@
+let num=Math.floor(Math.random()*6)+1;
+console.log(num);
+
+const obj={
+    name:"BMW",
+    model:"2026A",
+    color:"Black"
+};
+console.log(obj.name);
+
+const Person={
+    name:"Karan",
+    age:23,
+    city:"New York"
+};
